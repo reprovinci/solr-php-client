03e0a6df1bc168b1d52956ac608df0109660858a
Reinier Kip <dev@rjkip.nl>
2014-04-24 15:43:59 +0200

    Change <add> XML fragment to conform to Solr 4 specs.

--- Apache/Solr/Compatibility/Solr4CompatibilityLayer.php
+++ Apache/Solr/Compatibility/Solr4CompatibilityLayer.php
@@ -1,6 +1,8 @@
 <?php
 
-class Apache_Solr_Compatibility_Solr4CompatibilityLayer implements Apache_Solr_Compatibility_CompatibilityLayer
+class Apache_Solr_Compatibility_Solr4CompatibilityLayer implements
+	Apache_Solr_Compatibility_CompatibilityLayer,
+	Apache_Solr_Compatibility_AddDocumentXmlCreator
 {
 	/**
 	 * Creates a commit command XML string.
@@ -38,6 +40,37 @@ class Apache_Solr_Compatibility_Solr4CompatibilityLayer implements Apache_Solr_C
 		$rawPost = '<optimize waitSearcher="' . $searcherValue . '" />';
 
 		return $rawPost;
+	}
+
+	/**
+	 * Creates an add command XML string
+	 *
+	 * @param string  $rawDocuments String containing XML representation of documents.
+	 * @param boolean $allowDups
+	 * @param boolean $overwritePending
+	 * @param boolean $overwriteCommitted
+	 * @param integer $commitWithin The number of milliseconds that a document must be committed within,
+	 *        see @{link http://wiki.apache.org/solr/UpdateXmlMessages#The_Update_Schema} for details. If left empty
+	 *        this property will not be set in the request.
+	 * @return string An XML string
+	 */
+	public function createAddDocumentXmlFragment(
+		$rawDocuments,
+		$allowDups = false,
+		$overwritePending = true,
+		$overwriteCommitted = true,
+		$commitWithin = 0
+	) {
+		$dupValue = !$allowDups ? 'true' : 'false';
+
+		$commitWithin = (int) $commitWithin;
+		$commitWithinString = $commitWithin > 0 ? " commitWithin=\"{$commitWithin}\"" : '';
+
+		$addXmlFragment = "<add overwrite=\"{$dupValue}\"{$commitWithinString}>";
+		$addXmlFragment .= $rawDocuments;
+		$addXmlFragment .= '</add>';
+
+		return $addXmlFragment;
 	}
 }
 
